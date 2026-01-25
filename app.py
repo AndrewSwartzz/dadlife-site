d99f88c52c3c5382bb14729fbaea1d1d77939f06
andrew_swartzz <andreww_swartzz@gmail.com>
2026-01-25 13:18:38 -0500

    updated home

--- app.py
+++ app.py
@@ -211,9 +211,12 @@ def projects():
 def book():
     return render_template("book.html")
 
+import os
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=10000)
 
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
